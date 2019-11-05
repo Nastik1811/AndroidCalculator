ecd702336589cb52a837f9378147111120200d72
Anastasia <lugovskaya.1999@mail.ru>
2019-11-05 21:11:21 +0300

    Created all fragments

--- app/src/main/java/com/example/calculator/ScientificOptionsFragment.kt
+++ app/src/main/java/com/example/calculator/ScientificOptionsFragment.kt
@@ -8,20 +8,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Activities that contain this fragment must implement the
- * [DataPaneFragment.OnFragmentInteractionListener] interface
+ * [ScientificOptionsFragment.OnFragmentInteractionListener] interface
  * to handle interaction events.
- * Use the [DataPaneFragment.newInstance] factory method to
+ * Use the [ScientificOptionsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class DataPaneFragment : Fragment() {
+class ScientificOptionsFragment : Fragment() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +28,6 @@ class DataPaneFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_data_pane, container, false)
+        return inflater.inflate(R.layout.fragment_scientific_options, container, false)
     }
-
 }
